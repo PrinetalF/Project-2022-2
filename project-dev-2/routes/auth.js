218fa983a7ae036ffc583998533295b89ee65d0a
Pannawich Khongtoem <pannawich250@gmail.com>
2023-03-12 19:47:51 +0700

    Update auth.js

--- project-dev-2/routes/auth.js
+++ project-dev-2/routes/auth.js
@@ -1,29 +1,28 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
 
 const User = require('../models/User');
 
 router.post('/register', async (req, res) => {
-  const user = new User(req.body);
+  const { username, password, name } = req.body;
+
+  // simple validation
+  if (!name || !username || !password) {
+    return res.render('register', { message: 'Please try again' });
+  }
+
+  const passwordHash = bcrypt.hashSync(password, 10);
+  const user = new User({
+    name,
+    username,
+    password: passwordHash
+  });
+
   await user.save();
   res.render('index', { user });
 });
 
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-
-  const user = await User.findOne({
-    username,
-    password
-  });
-
-  if (user) {
-    return res.render('index', { user });
-  } else {
-    return res.render('login', { message: 'Email or Password incorrect' });
-  }
-});
-
-module.exports = router;
 
 
