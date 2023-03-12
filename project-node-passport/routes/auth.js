3119233a4dc37504dcf580dd3809daf938bdf11d
Pannawich Khongtoem <pannawich250@gmail.com>
2023-03-12 15:33:35 +0700

    Update auth.js

--- project-node-passport/routes/auth.js
+++ project-node-passport/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/User');
+const User = require('../model/User');
 
 router.post('/register', async (req, res) => {
   const user = new User(req.body);
